b2f26555da64f5723fe3781b48e5b5f02c2c3d66
gordonyli <gordonyli@gmail.com>
2017-03-04 14:42:08 -0500

    browse page started

--- thrifti/src/pages/browse-page/browse-page.ts
+++ thrifti/src/pages/browse-page/browse-page.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-page1',
+  selector: 'page-browsePage',
   templateUrl: 'browse-page.html'
 })
-export class Page1 {
+export class BrowsePage {
 
   constructor(public navCtrl: NavController) {
 
