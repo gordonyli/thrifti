ddb8106e90498f64e753b9db1928cf072ff01341
Bruck Woldeyes <Bruck Woldeyes>
2017-03-04 22:01:51 -0500

    switching between pages when clicking button

--- thrifti/src/pages/page1/page1.ts
+++ thrifti/src/pages/page1/page1.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { BrowsePage} from '../browse-page/browse-page';
+import { Page2 } from '../page2/page2';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
 
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
-
+  page2 = Page2; 
+  browsePage = BrowsePage;
   constructor(public navCtrl: NavController) {
     
   }
